d45c023b5f3ae66e2b051117f469d359fb375588
S.Tharun Kumar <150583912+Tharun-Roy19@users.noreply.github.com>
2026-06-29 20:21:30 +0530

    Add files via upload

---
+++ Project Files/Project_deployment.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+import pickle
+import joblib
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[2]:
+
+
+# Here itself we dont using pickle.load because of github.
+final_model=joblib.load(open('Apple_project.pkl','rb'))
+
+
+# In[3]:
+
+
+st.title('Apple Stock Price Prediction')
+
+
+# In[4]:
+
+
+def user_input_parameter():
+
+    forecast_days = st.sidebar.slider("Select Forecast Days",min_value=1,max_value=30,value=7)
+    data = {'Forecast_Days': forecast_days}
+    return forecast_days
+
+forecast_days=user_input_parameter()
+final_fit=final_model.fit()
+fut_pred=final_fit.forecast(steps=forecast_days)
+forecast_df = fut_pred.to_frame(name="Forecasted Price")
+
+st.subheader("Future Stock Price Forecast")
+st.dataframe(fut_pred)
+st.subheader('Forecast Graph')
+st.line_chart(fut_pred)
+
